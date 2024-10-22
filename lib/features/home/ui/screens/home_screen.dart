2f03ace9c9d45cf19a8133a71d64e98a1382f533
NourhanHamada <nourhan.hamada996@gmail.com>
2024-10-22 18:11:45 +0300

    delete switch language test widget

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -5,9 +5,6 @@ import 'package:mentorship/features/home/ui/widgets/build_products_grid_view.dar
 import 'package:mentorship/features/home/ui/widgets/home_header.dart';
 import 'package:mentorship/features/home/ui/widgets/home_search_bar_with_filter.dart';
 
-import '../../../../core/Localization/localization_cubit.dart';
-import '../../../../core/di/dependency_injection.dart';
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -24,28 +21,6 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: (){
-                      getIt<LocalizationCubit>().chooseEnglishLanguage();
-                      // context.pushAndRemoveUtilsNamed(Routes.splashScreen);
-                    },
-                    child: Text('ENGLISH'),
-                  ),
-                  GestureDetector(
-                    onTap: (){
-                      getIt<LocalizationCubit>().chooseArabicLanguage();
-                      // context.pushAndRemoveUtilsNamed(Routes.splashScreen);
-                    },
-                    child: Text('ARABIC'),
-                  ),
-                ],
-              ),
-
-
               HomeHeader(),
               SearchBarWithFilter(),
               BannersSection(),
